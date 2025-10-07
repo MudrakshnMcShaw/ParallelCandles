bdd12cd29b4f6f473b86df2a5a48b59f6dce01d3
root <root@e2e-29-202.ssdcloudindia.net>
2025-10-07 18:49:43 +0530

    fixed python2 --> python3 in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 
 
 cd accelpix
-python2 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r r.txt
 
 cd ..
 cd truedata
-python2 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r r.txt
